b88cbd6d2473862be8a49f4dd2ea85681faed1cf
FinnHuelsbusch <f.huelsbusch@outlook.de>
2023-02-25 14:05:29 +0100

    added first two possebileties

--- 01/exercise01-bandit.py
+++ 01/exercise01-bandit.py
@@ -10,14 +10,15 @@ env = gym.make('BanditTenArmedGaussian-v0')
 print('observation space:', env.observation_space.n, 'dimensional')
 print('action space:', env.action_space.n, 'dimensional')
 
-env.seed(34) # make each run the same 
+env.seed(4711)
 observation = env.reset()
 
 rewards = []
 average_rewards = np.zeros(env.action_space.n)
 nr_steps_per_action = np.zeros(env.action_space.n)
 
-for i_episode in range(5):
+action_reward_dict = {}
+for i_episode in range(5000):
   
     print("episode Number is", i_episode)   
     
@@ -25,14 +26,18 @@ for i_episode in range(5):
     action = i_episode % env.action_space.n
         
     print("action is", action)
-
-    ### your code goes here ###
-    pass
-    ###########################
+    if i_episode < 1000: 
+        action = env.action_space.sample()
+    else: 
+        help = {k: np.average(v) for k, v in action_reward_dict.items()}
+        action = max(help.items(), key=lambda k: k[1])[0]
         
     # here we taking the next "step" in our environment by taking in our action variable randomly selected above
     observation, reward, done, info = env.step(action) 
     rewards.append(reward)
+    action_reward = action_reward_dict.get(action, [])
+    action_reward.append(reward)
+    action_reward_dict[action] = action_reward
 
     print("observation space is: ",observation)
     print("reward variable is: ",reward)
