938c144582269cd795b718acb8592f3ddeddef9a
FinnHuelsbusch <f.huelsbusch@outlook.de>
2023-02-27 10:41:47 +0100

    added comments and made exploration determenistic

--- 01/exercise01-bandit.py
+++ 01/exercise01-bandit.py
@@ -5,49 +5,45 @@ import matplotlib.pyplot as plt
 import statistics
 
 np.random.seed(42) # make runs deterministic for numpy random number generator
-
 env = gym.make('BanditTenArmedGaussian-v0')
 
 print('observation space:', env.observation_space.n, 'dimensional')
 print('action space:', env.action_space.n, 'dimensional')
 seeds = [42,4711,11,22,33,44,55]
+# structure to save total rewards of the different seeds
 total_rewards = []
 for seed in seeds: 
     env.seed(seed)
     observation = env.reset()
-
     rewards = []
-    average_rewards = np.zeros(env.action_space.n)
-    nr_steps_per_action = np.zeros(env.action_space.n)
     # use the fact that we know our states
     action_reward_dict = {v: [0] for v in range(10)}
     probability_to_explore = 0.9
     discount_factor = 0.975
+    # save the best action over time
     best_action = []
+    # save the probabilety to explore over time
     probability = []
     for i_episode in range(5000):
-    
         print("episode Number is", i_episode)   
-        
+        # choos if the agent is explored randomly
         if np.random.random() < probability_to_explore:
-            action = env.action_space.sample()
+            action = np.random.randint(0,10)
         else: 
-            # use median instead of average or max in order to ignore outliers
+            # use the maximum median instead of average or max in order to ignore outliers
             action = max(action_reward_dict.items(), key=lambda k: statistics.median(k[1]))[0]
-
-
         # here we taking the next "step" in our environment by taking in our action variable randomly selected above
         observation, reward, done, info = env.step(action) 
         rewards.append(reward)
         action_reward = action_reward_dict.get(action, [])
         action_reward.append(reward)
         action_reward_dict[action] = action_reward
-
+        # get the best action and append it for plotting
         best_action.append(max(action_reward_dict.items(), key=lambda k: statistics.median(k[1]))[0])
+        # save the probability of exploreing
         probability.append(probability_to_explore)
-
+        # discount the probabilety of exploreing
         probability_to_explore = probability_to_explore * discount_factor   
-
         print("observation space is: ",observation)
         print("reward variable is: ",reward)
         print("done flag is: ",done)
