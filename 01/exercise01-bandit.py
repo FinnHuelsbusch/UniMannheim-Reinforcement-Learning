d8fd082aceee08258baf96f7252cb7f25ab5ca45
FinnHuelsbusch <f.huelsbusch@outlook.de>
2023-03-02 21:19:36 +0100

    Added task 2

--- 01/exercise01-bandit.py
+++ 01/exercise01-bandit.py
@@ -2,70 +2,71 @@ import numpy as np
 import gym
 import gym_bandits
 import matplotlib.pyplot as plt
-import statistics
 
 np.random.seed(42) # make runs deterministic for numpy random number generator
+
 env = gym.make('BanditTenArmedGaussian-v0')
 
 print('observation space:', env.observation_space.n, 'dimensional')
 print('action space:', env.action_space.n, 'dimensional')
-seeds = [42,4711,11,22,33,44,55]
-# structure to save total rewards of the different seeds
-total_rewards = []
-for seed in seeds: 
-    env.seed(seed)
-    observation = env.reset()
-    rewards = []
-    # use the fact that we know our states
-    action_reward_dict = {v: [0] for v in range(10)}
-    probability_to_explore = 0.9
-    discount_factor = 0.975
-    # save the best action over time
-    best_action = []
-    # save the probabilety to explore over time
-    probability = []
-    for i_episode in range(5000):
-        print("episode Number is", i_episode)   
-        # choos if the agent is explored randomly
-        if np.random.random() < probability_to_explore:
-            action = np.random.randint(0,10)
-        else: 
-            # use the maximum median instead of average or max in order to ignore outliers
-            action = max(action_reward_dict.items(), key=lambda k: statistics.median(k[1]))[0]
-        # here we taking the next "step" in our environment by taking in our action variable randomly selected above
-        observation, reward, done, info = env.step(action) 
-        rewards.append(reward)
-        action_reward = action_reward_dict.get(action, [])
-        action_reward.append(reward)
-        action_reward_dict[action] = action_reward
-        # get the best action and append it for plotting
-        best_action.append(max(action_reward_dict.items(), key=lambda k: statistics.median(k[1]))[0])
-        # save the probability of exploreing
-        probability.append(probability_to_explore)
-        # discount the probabilety of exploreing
-        probability_to_explore = probability_to_explore * discount_factor   
-        print("observation space is: ",observation)
-        print("reward variable is: ",reward)
-        print("done flag is: ",done)
-        print("info variable is: ",info)
 
-    print("sum of rewards: " + str(np.sum(rewards)))
-    total_rewards.append(int(np.sum(rewards)))
+env.seed(34) # make each run the same 
+observation = env.reset()
 
-    plt.plot(rewards)
-    plt.ylabel('rewards')
-    plt.xlabel('steps')
-    plt.show()
+rewards = []
+average_rewards = np.zeros(env.action_space.n)
+nr_steps_per_action = np.zeros(env.action_space.n)
 
-    fig, ax1 = plt.subplots() 
-    ax1.set_xlabel('steps') 
-    ax1.set_ylabel('action', color = 'red') 
-    ax1.plot(best_action, color = 'red') 
-    ax1.tick_params(axis ='y', labelcolor = 'red') 
-    # Adding Twin Axes
-    ax2 = ax1.twinx() 
-    ax2.set_ylabel('probability to explore', color = 'blue') 
-    ax2.plot(probability, color = 'blue') 
-    ax2.tick_params(axis ='y', labelcolor = 'blue') 
-    plt.show()
-print(total_rewards)
+steps = 2000
+k = 30 # exploration prob. decay factor: can be optimized
+for i_episode in range(steps):
+  
+    print("episode Number is", i_episode)   
+    
+    #action = env.action_space.sample() # sampling the "action" array which in this case only contains 10 "options" because there is 10 bandits
+    #action = i_episode % env.action_space.n
+    #print("action is", action)
+
+    ### your code goes here ###
+    # We have to trade-off Exploitation and Exploration.
+    # For that we consider: 1) The expected best (action) value, 2) The uncertainty of all (action) values, 3) the remaining iterations
+    # We would like to explore early and exploit later in the episode
+    # Since all bandits are Gaussian, we estimate the Gaussian parameters (μ,σ) using the MLE (sample mean & sample variance)
+    # Idea: q differs depending on the #of iterations (explore early, exploit later)
+    # Exploration: Sample from bandit with highest variance (what if variance differs???)
+    # Exploitation: Pick action with highest expected value
+
+    def update_exected_value(action):
+        nr_steps_per_action[action]+=1
+        average_rewards[action] = (average_rewards[action] * (nr_steps_per_action[action]-1) + rewards[-1]) / nr_steps_per_action[action]
+
+    # prob of exploration
+    probability_to_explore = np.exp(-i_episode/(steps/k))
+
+    # choose an action
+    exploring = np.random.binomial(n=1,p=probability_to_explore) == 1
+        
+    if exploring:
+        action = env.action_space.sample()
+        print("here")
+    else:
+        action = np.argmax(average_rewards)
+        
+    # here we taking the next "step" in our environment by taking in our action variable randomly selected above
+    observation, reward, done, info = env.step(action) 
+    rewards.append(reward)
+
+    update_exected_value(action)
+    print("action", action)
+
+    #print("observation space is: ",observation)
+    print("reward variable is: ",reward)
+    #print("done flag is: ",done)
+    #print("info variable is: ",info)
+
+print("sum of rewards: " + str(np.sum(rewards)))
+
+plt.plot(rewards)
+plt.ylabel('rewards')
+plt.xlabel('steps')
+plt.show()
--- 01/exercise01-beer.py
+++ 01/exercise01-beer.py
@@ -42,35 +42,59 @@ P_down = np.array((
     [ 0    , 0  , 0  , 0  , 0  ,  0  , 1  , 0  ], # G     #  
     [ 0    , 0  , 0  , 0  , 0  ,  0  , 1  , 0  ], # B     #    
     [ 0    , 0  , 0  , 0  , 0  ,  0  , 0  , 1  ], # Li    #  
-    [ 0    , 0  , 0  , 0  , 0  ,  0  , 0  , 1  ], # F     #   
+    [ 0    , 0  , 0  , 0  , 0  ,  0  , 0  , 0  ], # F     #   
     [ 0    , 0  , 0  , 0  , 0  ,  0  , 0  , 1  ], # End   #   
 ))
 
-# For all first three questions: after computing state values always print them out!
 
 # Question 1:
 # compute the state value by matrix inversion
-# compute state value for always choosing up. 
-state_value_function_up = np.linalg.inv(np.identity(8) - 0.9 * P_up) * r[0]
-# compute state value for always choosing down.
-state_value_function_down = np.linalg.inv(np.identity(8) - 0.9 * P_down) * r[1]
-# compute the combined state_value_function
-state_value_function= 0.5 * state_value_function_up + 0.5 * state_value_function_down
-print(state_value_function)
-# compute state transitions for the 50/50 policy
-...
-# compute expected rewards for the 50/50 policy
-...
-# compute state values
-...
-######################
 
+### your code here ###
+pi_up = pi_down = 0.5
+
+# compute state transitions for the 50/50 policy
+state_transitions = P_up*pi_up + P_down*pi_down
+print(state_transitions)
+# compute expected rewards for the 50/50 policy
+expected_reward = r[0]*pi_up + r[1]*pi_down
+print(expected_reward)
+
+# compute state values
+state_values = np.linalg.inv(np.identity(8) - gamma*state_transitions) @ expected_reward
+print(state_values)
+######################
+print()
+print()
 # Question 2:
 # compute the state values of the 50/50 policy by Richardson iteration.
 # Let the iteration run so long as the nomr of the state values vector does not change by more than 0.001
+v = np.zeros(8)
 
-### your code here ###
-...
+delta = 0.001
+terminate = False
+while not terminate: 
+    Delta = np.float64(0) 
+    # reversed in order to terminate in one iteration. 
+    for state_index, s in reversed(list(enumerate(v))): # sweep through space by traversing from end to start state
+        v_old = s
+        # Up
+        inner_sum = 0
+        for next_state_index, next_state_prob in enumerate(P_up[state_index]):
+            inner_sum += next_state_prob*v[next_state_index]
+        s = 0.5 * (r[0][state_index] + gamma * inner_sum)
+        # Down 
+        inner_sum = 0
+        for next_state_index, next_state_prob in enumerate(P_down[state_index]):
+            inner_sum += next_state_prob*v[next_state_index]
+        s += 0.5 * (r[1][state_index] + gamma * inner_sum )
+        v[state_index] = s
+        Delta = np.max([Delta, np.abs(s - v_old)])
+        print(state_index, s, v, v_old)
+        # terminate?
+        if Delta < delta:
+            terminate = True
+print(v)
 ######################
 
 # Question 3:
