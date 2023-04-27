66e66f8e99922d229bd998007d7f51f0612f5fe0
FinnHuelsbusch <f.huelsbusch@outlook.de>
2023-04-27 16:46:12 +0200

    rename file

--- 04/exercise04-dqn.py
+++ 04/exercise04-dqn.py
@@ -16,23 +16,25 @@ from collections import namedtuple
 from typing import Iterator, List, Tuple
 import collections
 import copy
+import time
 import random
 from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
 import ale_py
 from ale_py import ALEInterface
+
+seed = 4711
 
 ale = ALEInterface()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
-
 # In this exercise we will use deep Q-learning to train an agent to play the Atari game of breakout.
 # We will use some ideas from the Rainbow DQN paper to improve upon the original DQN paper from Mnih.
 
 # Notes:
 # (i) the training this time will be time-consuming! You should start coding pretty fast so that you will have enough time to train your agent.
-# (ii) in order to get good results, the replay buffer for DQN needs to be pretty large. You will need a machine with 8GB RAM at least.
-# (iii) You will need many training iterations until your model picks up anything useful. If in the beginning your model does not appear to learn, this might be the reason.
+# (ii) The replay buffer does not need to be as large as in the DQN paper for breakout. 100000 is enough.
 
 
 
@@ -84,21 +86,24 @@ class ReplayBuffer:
         indices = np.random.choice(len(self.buffer), batch_size, replace=False)
         states, actions, rewards, dones, next_states = zip(*[self.buffer[idx] for idx in indices])
 
-        return (np.array(states), np.array(actions, dtype=np.int64), np.array(rewards, dtype=np.float32),
+        return (np.array(states), np.array(actions), np.array(rewards, dtype=np.float32),
                 np.array(dones, dtype=bool), np.array(next_states))
 
 def reset(env):
-    env.reset()
+    env.reset(seed=seed)
     state, _, done, truncated, _ = env.step(env.action_space.sample())
     if done or truncated:
         return reset(env)
     return np.array(state)
 
+def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
+    slope = (end_e - start_e) / duration
+    return max(slope * t + start_e, end_e)
 
-def DQN(qnet, env, optimizer, epsilon=0.1, gamma=1.0, nr_episodes=5000, max_t=100, 
+def DQN(qnet, env, optimizer, start_epsilon=1, end_epsilon = 0.05, exploration_fraction = 0.1, gamma=1.0, nr_episodes=5000, max_t=100, 
         replay_buffer_size=1000000, batch_size=32, warm_start_steps=1000, sync_rate=1024, train_frequency=8):
     
-    print(f"Train policy with DQN for {nr_episodes} episodes using at most {max_t} steps, gamma = {gamma}, epsilon = {epsilon}, replay buffer size = {replay_buffer_size}, sync rate = {sync_rate}, warm starting steps for filling the replay buffer = {warm_start_steps}")
+    print(f"Train policy with DQN for {nr_episodes} episodes using at most {max_t} steps, gamma = {gamma}, start epsilon = {start_epsilon}, end epsilon = {end_epsilon}, exploration fraction = {exploration_fraction}, replay buffer size = {replay_buffer_size}, sync rate = {sync_rate}, warm starting steps for filling the replay buffer = {warm_start_steps}")
 
     target_qnet = copy.deepcopy(qnet)
     buffer = ReplayBuffer(replay_buffer_size)
@@ -106,6 +111,15 @@ def DQN(qnet, env, optimizer, epsilon=0.1, gamma=1.0, nr_episodes=5000, max_t=10
     episode_returns = []
     episode_lengths = []
     nr_terminal_states = []
+
+    run_name = f"breakout__{int(time.time())}"
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        f"|start_epsilon|{start_epsilon}||end_epsilon|{end_epsilon}||gamma|{gamma}||replay_buffer_size|{replay_buffer_size}||batch_size|{batch_size}|train_frequency|{train_frequency}||sync_rate|{sync_rate}"
+    )
+
+    start_time = time.time()
 
     # populate buffer
     state = reset(env)
@@ -125,6 +139,7 @@ def DQN(qnet, env, optimizer, epsilon=0.1, gamma=1.0, nr_episodes=5000, max_t=10
         for e in tepisodes:
             state = reset(env)
             episode_return = 0.0
+            epsilon = linear_schedule(start_epsilon, end_epsilon, exploration_fraction * nr_episodes, e)
 
             # Collect trajectory
             for t in range(max_t):
@@ -156,6 +171,10 @@ def DQN(qnet, env, optimizer, epsilon=0.1, gamma=1.0, nr_episodes=5000, max_t=10
                     expected_qvalues = gamma * next_qvalues + torch.Tensor(rewards).to(device)
                     loss = nn.MSELoss()(qvalues, expected_qvalues)
 
+                    writer.add_scalar("losses/td_loss", loss, step_counter)
+                    writer.add_scalar("losses/q_values", qvalues.mean().item(), step_counter)
+                    writer.add_scalar("charts/SPS", int(step_counter / (time.time() - start_time)), step_counter)
+
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
@@ -164,21 +183,24 @@ def DQN(qnet, env, optimizer, epsilon=0.1, gamma=1.0, nr_episodes=5000, max_t=10
                 if step_counter % sync_rate == 0:
                     target_qnet.load_state_dict(qnet.state_dict())
 
-                if done:
+                if done or truncated:
                     break
 
             episode_lengths.append(t+1)
             episode_returns.append(episode_return)
 
-            # if e % 200 == 0:
-            #     gym_video(qnet, env, filename=f"dqn_breakout_{e}", nr_steps=1000)
+            writer.add_scalar("charts/episodic_return", episode_returns[-1], step_counter)
+            writer.add_scalar("charts/episodic_length", episode_lengths[-1], step_counter)
+            writer.add_scalar("charts/epsilon", epsilon, step_counter)
 
             tepisodes.set_postfix({
-                'mean episode reward': "{:3.2f}".format(np.mean(episode_returns[-25:])),
+                'mean episode return': "{:3.2f}".format(np.mean(episode_returns[-25:])),
                 'mean episode length': "{:3.2f}".format(np.mean(episode_lengths[-25:])),
                 'nr terminal states in batch': "{:3.2f}".format(np.mean(nr_terminal_states[-25:])),
                 'global step': step_counter,
             })
+    env.close()
+    writer.close()
 
 
 # network: Use a small CNN of the following type:
@@ -209,12 +231,8 @@ class Model(nn.Module):
         x = F.leaky_relu(self.fc1(torch.flatten(x,-3,-1)), 0.01)
         x = self.fc2(x)
         return x
-    
-    def act(self, x):
-        with torch.no_grad():
-            q_values = self.forward(x)
-            action = torch.argmax(q_values).item()
-            return action, q_values
+
+
 
 env = gym.make('BreakoutNoFrameskip-v4', render_mode='rgb_array')
 #env = gym.wrappers.RecordEpisodeStatistics(env)
@@ -229,26 +247,30 @@ env = gym.wrappers.ResizeObservation(env, (84, 84))
 env = gym.wrappers.GrayScaleObservation(env)
 env = gym.wrappers.FrameStack(env, 4)
 env = gym.wrappers.RecordVideo(env, f"videos/", episode_trigger=lambda episode: episode % 200 == 0)
-#env.seed(seed)
-#env.action_space.seed(seed)
-#env.observation_space.seed(seed)
+
+env.action_space.seed(seed)
+env.observation_space.seed(seed)
+np.random.seed = seed
+torch.manual_seed = seed
 
 print('gym:', gym.__version__)
 print('ale_py:', ale_py.__version__)
 
-model = Model(env.action_space.n).to(device)
 
-env.reset()
+env.reset(seed=4711)
 env.step(0)
 env.render()
 
-epsilon = 0.1
-nr_episodes = 2000000
+start_epsilon = 0.4
+end_epsilon = 0.01
+exploration_fraction = 0.1
+nr_episodes = 20000
 max_t = 4000
 gamma = 0.99
-replay_buffer_size = 200000 # 1M is the DQN paper default
+replay_buffer_size = 100000 # 1M is the DQN paper default
 
-#optimizer = torch.optim.Adam(model.parameters(), lr=1e-5 , eps=1.5e-4) 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+model = Model(env.action_space.n).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0000625 , eps=1.5e-4) 
+#optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 #optimizer = torch.optim.RMSprop(model.parameters(), lr=1e-2)
-DQN(model, env, optimizer, gamma=gamma, epsilon=epsilon, nr_episodes=nr_episodes, max_t=max_t, warm_start_steps=500, sync_rate=128, replay_buffer_size=replay_buffer_size, train_frequency=8)
+DQN(model, env, optimizer, gamma=gamma, start_epsilon=start_epsilon, end_epsilon=end_epsilon, exploration_fraction=exploration_fraction, nr_episodes=nr_episodes, max_t=max_t, warm_start_steps=500, sync_rate=128, replay_buffer_size=replay_buffer_size, train_frequency=2)
